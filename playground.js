6f0d6e487f93cd458eb571493c28cef6a33c7c63
Phillip Halpin <phillip@rocketmatter.com>
2014-06-21 15:23:46 -0400

    - Split out runSpeedTests into analysis.js - Included analysis.js on index.html - Added another speed test for Deep Key Non-Indexed Multi-Finds

--- playground.js
+++ playground.js
@@ -1,11 +1,19 @@
 /**
  * Created by phalpin on 6/20/14.
  */
+var
+    objAmount = 5000,
+    testIndex = "test.id",
+    batchCreate = true,
+    batchUpdate = true,
+    batchDelete = true
+;
+
 var r = new Repo({
-    indexBy: "test.id",
-    createInBatch: true,
-    updateInBatch: true,
-    deleteInBatch: true,
+    indexBy: testIndex,
+    createInBatch: batchCreate,
+    updateInBatch: batchUpdate,
+    deleteInBatch: batchDelete,
     saveMethod: function(item, mode){
         switch(mode){
             case Repo.mode.Create:
@@ -17,7 +25,6 @@ var r = new Repo({
         }
     }
 });
-
 r.observe(function(i, mode){
     switch(mode){
         case Repo.mode.Added:
@@ -28,10 +35,7 @@ r.observe(function(i, mode){
             break;
     }
 });
-
-
-
-for(var i=0; i<50; i++){
+for(var i=0; i<objAmount; i++){
     r.push(new Item(i));
 }
 
@@ -70,63 +74,5 @@ runSpeedTests("Indexed Multi-Find", r, 'find', [[25,26,27,28,29,30]]);
 runSpeedTests("Non-Indexed Multi-Find", r, 'find', ["name", ["Test Object 25", "Test Object 26", "Test Object 27", "Test Object 28", "Test Object 29", "Test Object 30"]]);
 
 
-runSpeedTests("Deep Key Non-Indexed Find", r, 'find', ["deep.key.find.target.val.will.be.something.like", 26]);
-
-
-
-function runSpeedTests(name, obj, func, args){
-    var runs = 20;
-    var start, elapsed, output;
-    var times = [];
-
-    var time = {
-        Average: 0,
-        Max: 0,
-        Min: 0
-    };
-
-
-    function calculateTimes(){
-        var totalTime = 0;
-
-        //Time
-        for(var i=0; i<runs; i++){
-            totalTime += times[i];
-        }
-        time.Average = totalTime / runs;
-        time.Max = Math.max.apply(Math,times);
-        time.Min = Math.min.apply(Math, times);
-    }
-
-    function runTests(){
-        for(var i=0; i<runs; i++){
-            start = new Date();
-            output = obj[func].apply(obj, args);
-            elapsed = new Date() - start;
-            times.push(elapsed);
-        }
-
-    }
-
-
-
-    runTests();
-    calculateTimes();
-
-    console.log("Speed Test: " + name);
-    console.log("Result:", output);
-    console.log("Time Statistics (ms):", time);
-    console.log("");
-}
-/*
-start = new Date();
-console.log("Find Non-Indexed Test: ", r.find("name", "Test Object 25"));
-elapsed = new Date() - start;
-console.log("Time taken: ", elapsed);
-
-
-start = new Date();
-console.log("Find Test: ", r.find(25));
-elapsed = new Date() - start;
-console.log("Time taken: ", elapsed);
-*/
+runSpeedTests("Deep Key Non-Indexed Find", r, 'find', ["deep.key.find.target.val.will.be.something.like", 25]);
+runSpeedTests("Deep Key Non-Indexed Multi-Find", r, 'find', ["deep.key.find.target.val.will.be.something.like", [25,26,27,28,29,30]]);
---
+++ util/analysis.js
@@ -0,0 +1,51 @@
+/**
+ * Runs a set of speed tests against a particular function.
+ * @param name Name of test
+ * @param obj Object to run from the scope of
+ * @param func String name of the function to execute.
+ * @param args Arguments (array) to send to the function.
+ */
+function runSpeedTests(name, obj, func, args){
+    var runs = 20;
+    var start, elapsed, output;
+    var times = [];
+
+    var time = {
+        Average: 0,
+        Max: 0,
+        Min: 0
+    };
+
+
+    function calculateTimes(){
+        var totalTime = 0;
+
+        //Time
+        for(var i=0; i<runs; i++){
+            totalTime += times[i];
+        }
+        time.Average = totalTime / runs;
+        time.Max = Math.max.apply(Math,times);
+        time.Min = Math.min.apply(Math, times);
+    }
+
+    function runTests(){
+        for(var i=0; i<runs; i++){
+            start = new Date();
+            output = obj[func].apply(obj, args);
+            elapsed = new Date() - start;
+            times.push(elapsed);
+        }
+
+    }
+
+
+
+    runTests();
+    calculateTimes();
+
+    console.log("Speed Test: " + name);
+    console.log("Result:", output);
+    console.log("Time Statistics (ms):", time);
+    console.log("");
+}
